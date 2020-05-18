f1150e2f4b2966fa43bf9ba7cb7ba8256b54f689
20161104616 <992846873@qq.com>
2020-05-19 01:16:20 +0800

    Initial commit

---
+++ src/main/java/com/mydemo/demols/DemolsApplication.java
@@ -0,0 +1,13 @@
+package com.mydemo.demols;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemolsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mydemo/demols/DemolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mydemo.demols;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemolsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
